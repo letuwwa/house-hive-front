a2dafe0bed13b3ab3144ebbb1a022e44275ea9fd
oliver <oliverradivan@gmail.com>
2026-07-22 14:24:09 +0300

    edit

--- src/api/houses.js
+++
@@ -1,9 +0,0 @@
-import api from "./client.js";
-
-export function getHouse(house_Id) {
-  return api.get(`/api/v1/houses/${house_Id}`);
-}
-
-export function listHouseMembers(house_Id) {
-  return api.get(`/api/v1/house-members?house_id=${house_Id}`);
-}
